e304381994b57c546e62cf6af8ae69697f103955
Kyaw Lin <kokyawlin@gmail.com>
2024-08-28 21:50:17 +0800

    add event manager
    initialize event manager

---
+++ event-manager/src/test/java/edu/nus/microservice/event_manager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nus.microservice.event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
