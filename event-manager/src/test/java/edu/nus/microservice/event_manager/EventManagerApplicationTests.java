4b51b20f8f62b8c9545af40677971960c14d56e7
Kyaw Lin <kokyawlin@gmail.com>
2024-10-05 21:00:03 +0800

    Change Test Files
    Change Test Files

--- event-manager/src/test/java/edu/nus/microservice/event_manager/EventManagerApplicationTests.java
+++ event-manager/src/test/java/edu/nus/microservice/event_manager/EventManagerApplicationTests.java
@@ -1,19 +1,14 @@
 package edu.nus.microservice.event_manager;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class EventManagerApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
-	@Autowired
-	private ObjectMapper objectMapper;
+	@Test
+	void contextLoads() {
+	}
 
 }
